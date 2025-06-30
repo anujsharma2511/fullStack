45443fe33fbf814f82f3d6b1019a3794cf19ffad
anujsharma2511 <anuj8211@gmail.com>
2025-06-30 08:09:23 +0530

    ok

---
+++ test.js
@@ -0,0 +1,17 @@
+// npm init
+// Initialize a new Node.js project
+// npm init -y
+// -y flag to accept all defaults
+
+// Semantic versioning
+// MAJOR.MINOR.PATCH
+// Increment the MAJOR version for incompatible API changes
+// Increment the MINOR version for added functionality in a backwards-compatible manner
+// Increment the PATCH version for backwards-compatible bug fixes
+
+// Example of semantic versioning
+// Initial version 1.0.0
+
+// require vs import
+// require is used in CommonJS modules
+// import is used in ES6 modules
